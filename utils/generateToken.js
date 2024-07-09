c535f336ad9493ffe3044742413669849724944e
Sebastian Raju <sebzstianraju79@gmail.com>
2024-07-10 01:57:18 +0530

    changed generateToken

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -6,7 +6,7 @@ const generateToken = (res, userId) => {
   // set JWT as HTTP only cookie
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: false,
+    secure: process.env.NODE_ENV === 'development',
     sameSite: 'None',
     maxAge: 30 * 24 * 60 * 60 * 1000  // 30 days
   })
